521b0612412cd20c8ec2e94694ed560682690f6d
william koller <wkoller25@gmail.com>
2021-03-06 00:43:43 -0300

    feat: add LoadUserByEmailRepository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddUserRepository } from '@/user/repositories/add-user/add-user.repository';
 import { Hasher } from '@/shared/criptography/hasher';
 import { HashComparer } from '@/shared/criptography/hash-comparer';
+import { LoadUserByEmailRepository } from '@/user/repositories/load-user-by-email/load-user-by-email.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, LoadUserByEmailRepository])],
   providers: [AddUserRepository, Hasher, HashComparer],
 })
 export class UserModule {}
