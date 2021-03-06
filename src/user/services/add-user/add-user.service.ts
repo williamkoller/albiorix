395051f29720cd76b772f27f981878f636fd0498
william koller <wkoller25@gmail.com>
2021-03-06 01:35:01 -0300

    feat: add AddUserService and verify is user already exists with this email.

--- src/user/services/add-user/add-user.service.ts
+++ src/user/services/add-user/add-user.service.ts
@@ -1,1 +1,25 @@
-export class AddUserService {}
+import { User } from '@/entities/user';
+import { AddUserDto } from '@/user/dtos/add-user/add-user.dto';
+import { AddUserRepository } from '@/user/repositories/add-user/add-user.repository';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { LoadUserByEmailService } from '../load-user-by-email/load-user-by-email.service';
+
+@Injectable()
+export class AddUserService {
+  constructor(
+    private readonly addUserRepository: AddUserRepository,
+    private readonly loadUserByEmailService: LoadUserByEmailService,
+  ) {}
+
+  async addUser(addUserDto: AddUserDto): Promise<User> {
+    const user = await this.loadUserByEmailService.loadUserByEmail(
+      addUserDto.email,
+    );
+
+    if (user) {
+      throw new ConflictException('A user already exists with this email.');
+    }
+
+    return await this.addUserRepository.add(addUserDto);
+  }
+}
