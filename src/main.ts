26a4929ea78b562a8716cc45117b5f96bb08141a
william koller <wkoller25@gmail.com>
2021-03-05 22:59:32 -0300

    feat: add LoggingInterceptor and TransformInterceptor

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const logger = new Logger('Main');
 
-  app.setGlobalPrefix('api');
-
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+  );
+
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT, () => logger.log(`Server running...`));
 }
