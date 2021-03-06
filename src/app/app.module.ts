8e3b73f3021509a6f13824617329676fc00efef4
william koller <wkoller25@gmail.com>
2021-03-05 23:04:11 -0300

    feat: add throttler for rate limit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { configService } from '@/config/config.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({
+      ttl: Number(process.env.THROTTLER_TTL),
+      limit: Number(process.env.THROTTLER_LIMIT),
     }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
   ],
