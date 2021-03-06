c2f5aaf1e2a2caa46e05254f763eb821296ec20f
william koller <wkoller25@gmail.com>
2021-03-06 00:33:51 -0300

    feat: add seupt TypeOrm and set User, AddUserRepository, Hasher and HashComparer

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,12 @@
+import { User } from '@/entities/user';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddUserRepository } from '@/user/repositories/add-user/add-user.repository';
+import { Hasher } from '@/shared/criptography/hasher';
+import { HashComparer } from '@/shared/criptography/hash-comparer';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AddUserRepository, Hasher, HashComparer],
+})
 export class UserModule {}
