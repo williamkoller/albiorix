09a0a9c8167490be2bc503c786736a28b50ce532
william koller <wkoller25@gmail.com>
2021-03-19 01:07:18 -0300

    feat: add in AddCategoryController UseGuards(JwtAuthGuard)

--- src/category/controllers/add-category/add-category.controller.ts
+++ src/category/controllers/add-category/add-category.controller.ts
@@ -1,7 +1,14 @@
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 import { AddCategoryDto } from '@/category/dtos/add-category/add-category.dto';
 import { AddCategoryService } from '@/category/services/add-category/add-category.service';
 import { Category } from '@/entities/category';
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('category')
@@ -9,6 +16,7 @@ import { ApiTags } from '@nestjs/swagger';
 export class AddCategoryController {
   constructor(private readonly addCategoryService: AddCategoryService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async add(@Body() addCategory: AddCategoryDto): Promise<Category> {
     try {
