dca360d2969f91b941df56a857aca232ea6133f5
william koller <wkoller25@gmail.com>
2021-03-27 13:12:49 -0300

    refactor: add findOne in loadUserById

--- src/user/repositories/load-user-by-id/load-user-by-id.repository.ts
+++ src/user/repositories/load-user-by-id/load-user-by-id.repository.ts
@@ -4,9 +4,6 @@ import { EntityRepository, Repository } from 'typeorm';
 @EntityRepository(User)
 export class LoadUserByIdRepository extends Repository<User> {
   async loadUserById(id: string): Promise<User> {
-    return await this.createQueryBuilder('users')
-      .select()
-      .where('id = :id', { id })
-      .getOne();
+    return await this.findOne(id);
   }
 }
